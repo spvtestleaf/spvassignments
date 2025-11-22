c1170e6041911857fe3682d861ded197ea9b7224
Prassi <email2spv@gmail.com>
2025-11-22 08:28:57 -0500

    Add files via upload

---
+++ Week1_Assignments/PrimitiveDataType.js
@@ -0,0 +1,73 @@
+/* Assignment Details:
+Declare variables using `let` for different data types in JavaScript, and verify their types using `typeof`,
+including an uninitialized variable.
+Assignment Requirements:
+Create the following variables using let (not using var) and check their typeOf
+a) firstName
+b) companyName
+c) mobileNumber
+d) isAutomation
+e) hasPlaywright (do not assign)
+Print and confirm the values and data types
+Hints to Solve:
+Focus on initializing variables with different values, including a string, number, boolean, and leave one
+variable undefined to practice with `typeof`.
+Expected Outcome:
+Upon completion, you should be able to:
+- Grasp the concepts of different data types in JavaScript */
+
+/* var firstname= "Prassi"
+var companyName= "abcd"
+var mobileNumber=1234567890
+var isAutomation=true
+var hasPlaywright
+var population=1234n
+var accountnumber
+var number=1234
+
+console.log(firstname)
+console.log(typeof firstname)
+console.log(typeof companyName)
+console.log(typeof mobileNumber)
+console.log(typeof isAutomation)
+console.log(typeof hasPlaywright)
+console.log(typeof population)
+console.log(accountnumber);
+console.log(1234);
+ 
+
+var accountnumber
+//var _12345
+
+console.log(accountnumber);
+console.log(1234); */
+
+
+/* Session 1 Assignment
+ Create the following variables using let (not using var) and check their typeOf
+ a) firstName
+ b) companyName
+ c) mobileNumber
+ d) isAutomation
+ e) hasPlaywright (do not assign) */
+
+var firstName = "Prasanna"
+console.log(firstName);
+console.log(typeof firstName);
+
+var companyName =  `GOOGLE`
+console.log(companyName)
+console.log(typeof companyName);
+
+var mobileNumber = 9876543210
+console.log(mobileNumber)
+console.log(typeof mobileNumber)
+
+var isAutomation = false
+console.log(isAutomation)
+console.log(typeof isAutomation)
+
+var hasPlaywright
+console.log(hasPlaywright)
+console.log(typeof hasPlaywright)
+
