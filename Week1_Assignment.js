a6a10386be037404d4cf6b08eb3e94e0463d6f99
spvtestleaf <email2spv@gmail.com>
2025-11-21 19:59:32 -0500

    Add files via upload

--- Week1_Assignment.js
+++ Week1_Assignment.js
@@ -1,4 +1,20 @@
-//let browser;
+/* Week-1 Assignment Details:
+Assignment Requirements:
+Create two functions : launchBrowser, runTests where,
+ a) launchBrowser need to take input as browserName (string) and do not return any
+ - use if-else (chrome or otherwise)
+ - Print the value
+ b) runTests need to take input as testType (string) and do not return any
+ - use switch case (smoke, sanity, regression, default (smoke))
+ - Print the values
+Call that function from the javascript
+Hints to Solve:
+For `launchBrowser`, use `if-else` to check if `browserName` is "chrome" and print accordingly. For
+`runTests`, use a `switch` statement to handle different `testType` values, including a default case.
+Expected Outcome:
+Upon completion, you should be able to:
+- Understand and implement different conditional statements using Javascript  */
+
 function lauchbrowser(browser){
     if (browser === "Chrome")
         console.log("Launching Chrome Browser");
